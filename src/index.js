6cd9fe327998b6cb80a867931792b48bdbd02361
META_ASH\sh729 <ash7299@metaconnection.kr>
2022-10-27 11:42:59 +0900

    web3modal config 수정

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 import { Web3Modal } from '@web3modal/react'
+import {chains, providers} from "@web3modal/ethereum";
+
 const config = {
-  projectId: 'f6b3b3cdad6af9ce4e840bae063879a1',
   theme: "dark",
   accentColor: "default",
   ethereum: {
     appName: 'eth_token_swap',
+    chains: [chains.mainnet, chains.goerli, chains.polygon, chains.polygonMumbai],
+    providers: [
+      providers.walletConnectProvider({
+        projectId: "f6b3b3cdad6af9ce4e840bae063879a1",
+      }),
+    ],
     autoConnect: true
-  }
+  },
+  projectId: 'f6b3b3cdad6af9ce4e840bae063879a1',
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
