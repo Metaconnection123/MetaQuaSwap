32c49a170894d58057547872dfa8aaaa46195c67
META_ASH\sh729 <ash7299@metaconnection.kr>
2022-10-27 12:25:21 +0900

    다른 방식으로 web3Modal 적용

--- src/App.js
+++ src/App.js
@@ -3,10 +3,43 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import SwapPage from './components/Swap';
 import { ConnectButton, useAccount } from '@web3modal/react';
-function App() {
-  const { connected, address } = useAccount()
+import { Web3Modal } from "@web3modal/react";
+import { WalletConnectButton } from "./components/WalletConnectButton";
 
-  return connected ? <h1>{address ? address : 'none'} </h1> : <ConnectButton />
+
+const config = {
+  projectId: 'f6b3b3cdad6af9ce4e840bae063879a1',
+  theme: "dark",
+  accentColor: "default",
+  ethereum: {
+    appName: 'eth_token_swap',
+  },
+};
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <WalletConnectButton />
+        <Web3Modal config={config} />
+        <a
+          className="App-link"
+          href="https://github.com/WalletConnect/web3modal"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          WalletConnect/web3modal
+        </a>
+      </header>
+    </div>
+  );
+
+
+  // const { connected, address } = useAccount()
+
+  // return connected ? <h1>{address ? address : 'none'} </h1> : <ConnectButton />
+  ////////////////
   // return (
   //   <Router>
   //     <Routes>
--- src/index.js
+++ src/index.js
@@ -2,36 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import { Web3Modal } from '@web3modal/react'
-import {chains, providers} from "@web3modal/ethereum";
+// import { Web3Modal } from '@web3modal/react'
+// import {chains, providers} from "@web3modal/ethereum";
 
-const config = {
-  projectId: 'f6b3b3cdad6af9ce4e840bae063879a1',
-  theme: "dark",
-  accentColor: "default",
-  ethereum: {
-    appName: 'eth_token_swap',
-    chains: [
-      chains.mainnet, 
-      chains.goerli, 
-      chains.polygon, 
-      chains.polygonMumbai
-    ],
-    providers: [
-      providers.walletConnectProvider({
-        projectId: "f6b3b3cdad6af9ce4e840bae063879a1",
-      }),
-    ],
-    autoConnect: true
-  },
+// const config = {
+//   projectId: 'f6b3b3cdad6af9ce4e840bae063879a1',
+//   theme: "dark",
+//   accentColor: "default",
+//   ethereum: {
+//     appName: 'eth_token_swap',
+//     chains: [
+//       chains.mainnet, 
+//       chains.goerli, 
+//       chains.polygon, 
+//       chains.polygonMumbai
+//     ],
+//     providers: [
+//       providers.walletConnectProvider({
+//         projectId: "f6b3b3cdad6af9ce4e840bae063879a1",
+//       }),
+//     ],
+//     autoConnect: true
+//   },
 
-};
+// };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <>
         <App />
-        <Web3Modal config={config} />
+        {/* <Web3Modal config={config} /> */}
     </>
 
 );
